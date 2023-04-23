9e5244f4f62801827bdd5e64ea2f94dc704eb7d6
Rayal kc <kcrayalgw@gmail.com>
2023-04-23 19:58:18 +0545

    Didnt Work :(

--- employee_ml.py
+++ employee_ml.py
@@ -1,58 +1,35 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.impute import SimpleImputer
+from datetime import datetime, timedelta
+from sklearn.ensemble import RandomForestClassifier
 
-df = pd.read_csv("D:/Rayal/new_dataset.csv", names=['id', 'dateOfQuestion', 'checkInTime', 'checkOutTime', 'Name', 'Duration', 'status', 'user_id', 'dayOfQuestion'], skiprows=[0])
+df = pd.read_csv('D:/Rayal/Machine-Learning/new_dataset.csv')
 
-# Display the employee names and IDs
 print(df[['Name', 'user_id']].drop_duplicates().reset_index(drop=True))
 
-# Prompt user to select an employee
 emp_id = int(input("Select an employee by entering their ID: "))
 
-# Create a new dataset for the selected employee
-selected_df = df[df['user_id'] == emp_id]
+df = df[df['user_id'] == emp_id].reset_index(drop=True)
 
-# Save the new dataset to a CSV file
-selected_df.to_csv("selected_employee.csv", index=False)
+df = df[(df['status'] != 'Holiday') & (df['status'] != 'Absent')]
 
-# Read the dataset
-df = pd.read_csv("D:/Rayal/selected_employee.csv", names=['id', 'dateOfQuestion', 'checkInTime', 'checkOutTime', 'Name', 'Duration', 'status', 'user_id', 'dayOfQuestion'], skiprows=[0])
+day_map = {'Sunday': 0, 'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6}
 
-# Encode day of the week as one-hot vector
-day_dummies = pd.get_dummies(df['dayOfQuestion'], prefix='dayOfQuestion')
-df = pd.concat([df, day_dummies], axis=1)
+df['day_num'] = df['dayOfQuestion'].apply(lambda x: day_map[x])
 
-# Drop original dayOfQuestion column
-df.drop('dayOfQuestion', axis=1, inplace=True)
+df['late_flag'] = (df['status'] == 'Late')
 
-# Set the target variable
-y = df['status']
+if sum(df['late_flag']) > 0:
+    X = df.loc[df['late_flag'], ['day_num']]
+    y = df.loc[df['late_flag'], 'late_flag']
+    rfc = RandomForestClassifier(n_estimators=10, random_state=42)
+    rfc.fit(X, y)
 
-# Set the features
-X = df[['dayOfQuestion_Friday', 'dayOfQuestion_Monday', 'dayOfQuestion_Saturday', 'dayOfQuestion_Sunday', 'dayOfQuestion_Thursday', 'dayOfQuestion_Tuesday', 'dayOfQuestion_Wednesday', 'Duration']]
+    tomorrow = datetime.now() + timedelta(days=1)
+    tomorrow_day_of_week = tomorrow.strftime('%A')
+    tomorrow_day_num = day_map[tomorrow_day_of_week]
+    prob_late = rfc.predict_proba([[tomorrow_day_num]])[0][1] * 100
 
-# Impute missing values using the most frequent strategy
-imputer = SimpleImputer(strategy='most_frequent')
-X = imputer.fit_transform(X)
 
-# Train the decision tree classifier on the entire dataset
-clf = DecisionTreeClassifier()
-clf.fit(X, y)
-
-# Get the employee's data for the most recent day
-recent_data = df.iloc[-1][['dayOfQuestion_Friday', 'dayOfQuestion_Monday', 'dayOfQuestion_Saturday', 'dayOfQuestion_Sunday', 'dayOfQuestion_Thursday', 'dayOfQuestion_Tuesday', 'dayOfQuestion_Wednesday', 'Duration']]
-recent_data = recent_data.values.reshape(1, -1)
-
-# Make prediction for the employee for the most recent day
-prediction = clf.predict(recent_data)
-
-# Print the prediction
-if prediction == 1:
-    print("Employee is predicted to be late tomorrow.")
+    print(f"There is a {prob_late:.2f}% chance that the employee will be late tomorrow.")
 else:
-    print("Employee is predicted to be on time tomorrow.")
-    
-import os
-if os.path.exists("D:/Rayal/selected_employee.csv"):
-    os.remove("D:/Rayal/selected_employee.csv")
+    print("No late records found for this employee.")
