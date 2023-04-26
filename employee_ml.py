cb50f8d539b5ec106e61091e312ac6fe2e8a4923
Rayal kc <kcrayalgw@gmail.com>
2023-04-26 20:51:55 +0545

    can now filter data incorrectly

--- employee_ml.py
+++ employee_ml.py
@@ -1,32 +1,32 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 
-# Load data from CSV file
-data = pd.read_csv("D:/Rayal/Machine-Learning/new_dataset.csv")
+# Load the dataset into a pandas dataframe
+df = pd.read_csv("D:/Rayal/Machine-Learning/new_dataset.csv")
 
-# Convert dayOfQuestion column to corresponding numeric value
-data['dayOfQuestion'] = pd.to_datetime(data['dayOfQuestion']).dt.dayofweek
+# Get a list of unique employee names and IDs
+employees = df[["Name", "user_id"]].drop_duplicates()
 
-# Filter out rows with absent or holiday status
-data = data[data['status'].isin(['Late', 'Present'])]
+# Display the list of employees
+print("List of employees:")
+for i, row in employees.iterrows():
+    print(f"{i + 1}. {row['Name']} (ID: {row['user_id']})")
 
-# Define features and target
-X = data[['status', 'dayOfQuestion']]
-y = data['status']
+# Ask the user to select an employee
+selected_employee_id = input("Enter the ID of the employee you want to make a prediction for: ")
 
-# Train the model
-model = RandomForestClassifier()
-model.fit(X, y)
+# Filter the dataset to only include rows for the selected employee
+selected_employee_data = df.loc[df["user_id"] == int(selected_employee_id)]
 
-# Get the day of the week for tomorrow
-import datetime
-import calendar
-tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
-tomorrow_day_name = calendar.day_name[tomorrow.weekday()]
-tomorrow_day_num = list(calendar.day_name).index(tomorrow_day_name)
+# Filter out rows with "Leave", "Absent", and "Holiday" status
+selected_employee_data = selected_employee_data.loc[(selected_employee_data["status"] != "Leave") &
+                                                    (selected_employee_data["status"] != "Absent") &
+                                                    (selected_employee_data["status"] != "Holiday")]
 
-# Predict the probability of being late tomorrow
-prob_late = model.predict_proba([[tomorrow_day_num, 'Present']])[:, 1][0] * 100
+# Map dayOfQuestion to day_numeric
+selected_employee_data["day_numeric"] = selected_employee_data["dayOfQuestion"].apply(lambda x: pd.to_datetime(x, format="%A").dayofweek + 1)
 
-# Output the result
-print(f"There is a {prob_late:.2f}% chance that the employee will be late tomorrow.")
+# Map status to a binary value (0 for Present, 1 for Late)
+selected_employee_data["status_binary"] = selected_employee_data["status"].apply(lambda x: 0 if x == "Present" else 1)
+
+# Display the selected employee data
+print(selected_employee_data.head())
