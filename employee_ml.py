54ed3b83ae45f622ba98da1b0803fdc32e3ada89
Rayal kc <kcrayalgw@gmail.com>
2023-04-26 21:10:01 +0545

    WORKS!!!!!!!!!!!!!

--- employee_ml.py
+++ employee_ml.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
 
 # Load the dataset into a pandas dataframe
 df = pd.read_csv("D:/Rayal/Machine-Learning/new_dataset.csv")
@@ -28,5 +30,27 @@ selected_employee_data["day_numeric"] = selected_employee_data["dayOfQuestion"].
 # Map status to a binary value (0 for Present, 1 for Late)
 selected_employee_data["status_binary"] = selected_employee_data["status"].apply(lambda x: 0 if x == "Present" else 1)
 
-# Display the selected employee data
-print(selected_employee_data.head())
+# Split the data into training and testing sets
+X = selected_employee_data[["day_numeric"]]
+y = selected_employee_data["status_binary"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a random forest regressor on the training data
+regressor = RandomForestRegressor(n_estimators=100, random_state=42)
+regressor.fit(X_train, y_train)
+
+# Make predictions on the testing data
+y_pred = regressor.predict(X_test)
+
+# Calculate the mean absolute error
+mae = abs(y_test - y_pred).mean()
+
+# Print the mean absolute error
+print(f"Mean absolute error: {mae}")
+
+# Make a prediction for today
+today_day_numeric = pd.Timestamp.today().dayofweek + 1
+today_pred = regressor.predict([[today_day_numeric]])
+
+# Print the predicted chance of being late today
+print(f"Predicted chance of being late today: {today_pred[0]}")
